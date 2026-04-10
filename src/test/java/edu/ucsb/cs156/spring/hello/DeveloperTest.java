c9d0bf7bf8251ef53ced3a3295950ce9993657f8
Pau Marsa <paumarsa@ucsb.edu>
2026-04-09 17:48:20 -0700

    pm - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -27,5 +27,27 @@ public class DeveloperTest {
 
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
+     @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("MrPau13", Developer.getGithubId());
+    }
+
+    // @Test
+    // public void getTeam_returns_team_with_correct_name() {
+    //     Team  t = Developer.getTeam();
+    //     assertEquals("s26-12", t.getName());
+    // }
+
+    //  @Test
+    // public void getTeam_returns_team_with_correct_members() {
+    //     Team  t = Developer.getTeam();
+    //     assertTrue(t.getMembers().contains("Lorenzo"),"Team should contain Lorenzo");
+    //     assertTrue(t.getMembers().contains("Arjun S"),"Team should contain Arjun S");
+    //     assertTrue(t.getMembers().contains("Jay"),"Team should contain Jay");
+    //     assertTrue(t.getMembers().contains("Pengyu"),"Team should contain Pengyu");
+    //     assertTrue(t.getMembers().contains("Arjun M"),"Team should contain Arjun M");
+    //     assertTrue(t.getMembers().contains("Pau"),"Team should contain Pau");
+
+    // }
 
 }
