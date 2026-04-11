299e88194edf05c4d073725bbe10b06924f8fe3d
Pau Marsa <paumarsa@ucsb.edu>
2026-04-10 19:09:22 -0700

    pm - part3

--- src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/TeamTest.java
@@ -1,6 +1,7 @@
 package edu.ucsb.cs156.spring.hello;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -24,21 +25,52 @@ public class TeamTest {
     // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
     // 100% mutation coverage (all mutants timed out or killed)
     @Test
-    public void getTeam_returns_team_with_correct_name() {
-        Team  t = Developer.getTeam();
-        assertEquals("s26-12", t.getName());
+    public void toString_returns_correct_string() {
+        assertEquals("Team(name=test-team, members=[])", team.toString());
     }
 
-     @Test
-    public void getTeam_returns_team_with_correct_members() {
-        Team  t = Developer.getTeam();
-        assertTrue(t.getMembers().contains("Lorenzo"),"Team should contain Lorenzo");
-        assertTrue(t.getMembers().contains("Arjun S"),"Team should contain Arjun S");
-        assertTrue(t.getMembers().contains("Jay"),"Team should contain Jay");
-        assertTrue(t.getMembers().contains("Pengyu"),"Team should contain Pengyu");
-        assertTrue(t.getMembers().contains("Arjun M"),"Team should contain Arjun M");
-        assertTrue(t.getMembers().contains("Pau"),"Team should contain Pau");
-
+    // Case 1: These are the same object
+    @Test
+    public void equals_same_object() {
+        assertTrue(team.equals(team));
     }
 
+    // Case 2: The other object isn't even an instance of this class
+    @Test
+    public void equals_different_class() {
+        Object other = new Object();
+        assertFalse(team.equals(other));
+    }
+
+    // Case 3: Comparing all fields (True/True)
+    @Test
+    public void equals_same_name_and_members() {
+        Team otherTeam = new Team("test-team");
+        assertTrue(team.equals(otherTeam)); 
+    }
+
+    // Case 3: Comparing all fields (True/False)
+    @Test
+    public void equals_different_members() {
+        Team otherTeam = new Team("test-team");
+        otherTeam.addMember("foo");
+        assertFalse(team.equals(otherTeam)); 
+    }
+
+    // Case 3: Comparing all fields (False/True or False/False)
+    @Test
+    public void equals_different_name() {
+        Team otherTeam = new Team("different-team");
+        assertFalse(team.equals(otherTeam)); 
+    }
+
+    @Test
+    public void hashCode_returns_specific_value() {
+        Team t = new Team("test-team");
+        
+        // Temporarily set to 0 to force a failure and find the real hash code
+        int expectedResult = -1226298695; 
+        assertEquals(expectedResult, t.hashCode());
+    }
+    
 }
